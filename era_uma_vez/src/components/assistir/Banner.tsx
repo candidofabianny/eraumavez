2258aae6862c65cb1dc6210289261b8f66560c9c
candidofabianny <candidofabianny@gmail.com>
2024-04-02 13:03:21 -0300

    alterações finais

--- era_uma_vez/src/components/assistir/Banner.tsx
+++ era_uma_vez/src/components/assistir/Banner.tsx
@@ -7,7 +7,7 @@ const Banner = () => {
       className="bg-[#9B90D3] flex flex-col justify-center items-center min-h-[150px] w-full">
       <div className="flex flex-col items-center justify-center text-center text-white p-4">
         <Link href="/assistir" className="text-[1.3rem] md:text-[1.6rem]">
-        "Quando a gente se ajudam as peças se encaixam"
+        "Quando nos ajudamos, as peças se encaixam!"
         </Link>
         <h1 className="pt-4 text-[0.9rem] md:text-[1.2rem]">
         Dia 02 de abril, dia mundial de conscientização do autismo.
